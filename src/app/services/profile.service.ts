56cda80bbd19eaa1491930d2e71a17c9d0dc3f02
Kaysha Besi <kayshabesi@gmail.com>
2020-04-12 12:38:28 +0300

    made the api work with a client id and client secret

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username:string;
+  private clientid = 'c754092d930e1856c117';
+  private clientsecret = '0b1e6e2fbfbac9c3ee1abd1f8cc69216bc90d2d6'
+
+  constructor(private http:HttpClient) { 
+    console.log("service is now ready");
+    this.username = 'kayshabes1'
+  }
+
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  }
+  
 }
