e837504479d15162bf569495b22ce1722b3ae321
Kaysha Besi <kayshabesi@gmail.com>
2020-04-12 19:16:25 +0300

    added the repositoriesand links to the actual repos on github

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -20,5 +20,10 @@ export class ProfileService {
     return this.http.get<any>("https://api.github.com/users/" + this.username + "?client_id=" + 
     this.clientid + "&client_secret=" + this.clientsecret)
   }
+
+  getProfileRepos(){
+    return this.http.get<any>("https://api.github.com/users/" + this.username + "/repos?client_id=" + 
+    this.clientid + "&client_secret=" + this.clientsecret)
+  }
   
 }
