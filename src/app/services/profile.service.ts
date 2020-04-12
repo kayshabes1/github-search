d22c08e05f9b21b80d3a20d680405c54743b5716
Kaysha Besi <kayshabesi@gmail.com>
2020-04-12 15:03:38 +0300

    fixed bug

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,12 +13,12 @@ export class ProfileService {
   private clientsecret = '0b1e6e2fbfbac9c3ee1abd1f8cc69216bc90d2d6'
 
   constructor(private http:HttpClient) { 
-    console.log("service is now ready");
     this.username = 'kayshabes1'
   }
 
   getProfileInfo(){
-    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+    return this.http.get<any>("https://api.github.com/users/" + this.username + "?client_id=" + 
+    this.clientid + "&client_secret=" + this.clientsecret)
   }
   
 }
